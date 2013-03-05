cda4b6552d5aaca1128b26d0b280f8960f0e606f
Joe Mirizio <joe@joemirizio.com>
2013-03-05 00:52:37 -0500

    Added initial object detection. Added ImageProcessor file saves.

--- processors/image.py
+++ processors/image.py
@@ -11,15 +11,17 @@ class ImageProcessor(object):
 
 	def __init__(self, img_source, frame_type=0):
 		self.img_source = img_source
+		self.last_frame = None
 		self.__avg_frame = None
 		self.frame_type = self.frame_types[frame_type]
 	
 	def process(self):
-		frame = self.img_source.read()
+		self.last_frame = self.img_source.read()
 		if self.avg_frame is None:
-			self.avg_frame = frame
-		frame, self.avg_frame = processImage(frame, self.avg_frame, self.frame_type)
-		return frame
+			self.avg_frame = self.last_frame
+		#self.last_frame, self.avg_frame = processImage(self.last_frame, self.avg_frame, self.frame_type)
+		self.last_frame, self.avg_frame = findObjects(self.last_frame, self.avg_frame, self.frame_type)
+		return self.last_frame
 
 	@property
 	def avg_frame(self):
@@ -31,6 +33,9 @@ class ImageProcessor(object):
 		else:
 			self.__avg_frame = frame
 
+	def saveFrame(self, filename=""):
+		self.img_source.save(filename, self.last_frame)
+
 	def setFrameType(self, frame_type):
 		if isinstance(frame_type, basestring):
 			if frame_type in self.frame_types:
@@ -40,6 +45,32 @@ class ImageProcessor(object):
 		else:
 			self.frame_type = self.frame_types[frame_type]
 
+
+BLUE_MIN = np.array([90, 50, 50], np.uint8)
+BLUE_MAX = np.array([120, 255, 255], np.uint8)
+def findObjects(frame, avg_frame, frame_type=ImageProcessor.frame_types[0]):
+	blur_frame = cv.GaussianBlur(frame, (19, 19), 0)
+	hsv_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV)
+	thresh_frame = cv.inRange(hsv_frame, BLUE_MIN, BLUE_MAX)
+
+	# Calculate contours
+	bw_copy = thresh_frame.copy()
+	contours, hier = cv.findContours(bw_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+	# Draw countours and bounding boxes
+	main_frame = frame.copy()
+	for contour in contours:
+		rect = cv.boundingRect(contour)
+		top  = rect[0:2]
+		bot  = (rect[0] + rect[2], rect[1] + rect[3])
+		cv.rectangle(main_frame, top, bot, (255, 0, 0), 1)
+	cv.drawContours(main_frame, contours, -1, (0, 255, 0), -1)
+
+	frames = dict(zip(ImageProcessor.frame_types, (main_frame, frame, thresh_frame)))
+	out_frame = frames[frame_type]
+
+
+	return (out_frame, avg_frame)
 	
 def processImage(frame, avg_frame, frame_type=ImageProcessor.frame_types[0]):
 	# Blur and average with previous frames
