dd0f233c9beb03b9ff7f5dfc515912ea51af3465
Joe Mirizio <joe@joemirizio.com>
2013-03-09 15:02:40 -0500

    Added ImageFile load error handling.

--- image_sources/image_file.py
+++ image_sources/image_file.py
@@ -1,10 +1,13 @@
 import os
+import sys
 import cv2 as cv
 
 class ImageFile:
 	def __init__(self, filename):
 		self.name = os.path.splitext(filename)[0]
 		self.image = cv.imread(filename)
+		if self.image is None:
+			raise Exception('Invalid image file "%s"' % filename)
 		self.height, self.width = self.image.shape[:2]
 
 	def read(self):
