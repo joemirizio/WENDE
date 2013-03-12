c476533072d5a2068136ac9639bbe93795f34b81
Kyra Holmquest <kyra.holmquest@gmail.com>
2013-03-11 22:17:41 -0400

    Moved all background to init module, changed colors

--- display/tactical/tactical.py
+++ display/tactical/tactical.py
@@ -9,17 +9,7 @@ class TacticalDisplay(object):
                 h = 250
                 w = 500
                 self.canvas = tk.Canvas(self.display, bg="white", height=h, width=w)
-                self.canvas.pack()
-
-        def displayTarget(self, target):
-                logging.info("displayTarget: %s" % target)
-                #TODO Add display logic here - dots that follow the target - tracer     
-
-        def update(self):
-                # Initialize - background things
-                #coord = [10, 50, 240, 210]
-                #self.canvas.create_arc(coord, start=0, extent=150, fill="red")
-                #coordinates of arcs, Top Left x and y, Bottom Right x and y
+                
                 predictionTx = 0
                 predictionTy = 0
                 predictionBx = 500
@@ -38,9 +28,31 @@ class TacticalDisplay(object):
                 coordPre = predictionTx, predictionTy, predictionBx, predictionBy
                 coordAlert = alertTx, alertTy, alertBx, alertBy
                 coordSafe = safeTx, safeTy, safeBx, safeBy
-                Pre = self.canvas.create_arc(coordPre, start = 30, extent = 120, fill="yellow")
-                Alert = self.canvas.create_arc(coordAlert, start=30, extent=120, fill="red")
-                Safe = self.canvas.create_arc(coordSafe, start=30, extent=120, fill="green")
+                Pre = self.canvas.create_arc(coordPre, start = 30, extent = 120, fill="#33B5E5")
+                Alert = self.canvas.create_arc(coordAlert, start=30, extent=120, fill="#FF4444")
+                Safe = self.canvas.create_arc(coordSafe, start=30, extent=120, fill="#99CC00")
+                
+                self.canvas.pack()
+
+        def displayTarget(self, target):
+                logging.info("displayTarget: %s" % target)
+                #TODO Add display logic here - map real coord to gui coords
+                #self.canvas.create_line(prevCoord, fill="black")
+                Tx = 225
+                Ty = 125
+                Bx = 230
+                By = 130
+                prevCoord = Tx, Ty, Bx, By
+
+                #tracking circle
+                self.canvas.create_oval(Tx, Ty, Bx, By, fill="black")
+                
+
+        def update(self):
+                # Initialize - background things
+                #coord = [10, 50, 240, 210]
+                #self.canvas.create_arc(coord, start=0, extent=150, fill="red")
+                #coordinates of arcs, Top Left x and y, Bottom Right x and y
                 
                 for target in self.data_proc.targets:
                         self.displayTarget(target)
