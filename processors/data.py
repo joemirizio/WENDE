643ed6afd38080baa8751a44324470588ea01caf
Nick Luzod <nluzod@gmail.com>
2013-06-03 22:45:48 -0700

    Changed safe zone distance to 5ft.

--- processors/data.py
+++ processors/data.py
@@ -10,83 +10,82 @@ TRACK_THRESHOLD = 0.075
 POS_THRESHOLD = 0.05
 
 class DataProcessor(object):
-	
-	def __init__(self):
-		self.targets = []		
-		self.coverages = {}
+    
+    def __init__(self):
+        self.targets = []        
+        self.coverages = {}
 
-	def process(self, data, img_proc):
-		for contour in data:
-			area = cv.contourArea(contour)
-			if area > AREA_THRESHOLD:
-				center, radius = cv.minEnclosingCircle(contour)
-				# Compute offset
-				y = float(center[1]) / float(img_proc.img_source.height) * img_proc.coverage_size[1] 
-				y = img_proc.A + (img_proc.B * y) + (img_proc.C * y**2)
-				pos = ([float(center[0]) / float(img_proc.img_source.width) * img_proc.coverage_size[0] - (img_proc.coverage_size[0] / 2) + img_proc.coverage_offset[0],
-						img_proc.coverage_size[1] - y + img_proc.coverage_offset[1]])
+    def process(self, data, img_proc):
+        for contour in data:
+            area = cv.contourArea(contour)
+            if area > AREA_THRESHOLD:
+                center, radius = cv.minEnclosingCircle(contour)
+                # Compute offset
+                y = float(center[1]) / float(img_proc.img_source.height) * img_proc.coverage_size[1] 
+                y = img_proc.A + (img_proc.B * y) + (img_proc.C * y**2)
+                pos = ([float(center[0]) / float(img_proc.img_source.width) * img_proc.coverage_size[0] - (img_proc.coverage_size[0] / 2) + img_proc.coverage_offset[0],
+                        img_proc.coverage_size[1] - y + img_proc.coverage_offset[1]])
 
-				self.addTarget(Target(pos))
-		self.addCoverage(img_proc)
+                self.addTarget(Target(pos))
+        self.addCoverage(img_proc)
 
-	def addTarget(self, target):
-		isPresent = False
-		for tgt in self.targets:
-			dist = distance(target.pos, tgt.pos)
-			if dist < DETECT_THRESHOLD:
-				isPresent = True
-				if dist > TRACK_THRESHOLD:
-					tgt.recordPosition()
-				if dist > POS_THRESHOLD:
-					tgt.pos = target.pos
-		if not isPresent:
-			#logging.info("Adding target")
-			self.targets.append(target)
+    def addTarget(self, target):
+        isPresent = False
+        for tgt in self.targets:
+            dist = distance(target.pos, tgt.pos)
+            if dist < DETECT_THRESHOLD:
+                isPresent = True
+                if dist > TRACK_THRESHOLD:
+                    tgt.recordPosition()
+                if dist > POS_THRESHOLD:
+                    tgt.pos = target.pos
+        if not isPresent:
+            #logging.info("Adding target")
+            self.targets.append(target)
 
-	def addCoverage(self, img_proc):
-		self.coverages[img_proc] = [img_proc.coverage_size, img_proc.coverage_offset]
+    def addCoverage(self, img_proc):
+        self.coverages[img_proc] = [img_proc.coverage_size, img_proc.coverage_offset]
 
-	def clearTargetData(self):
-		self.targets = []
+    def clearTargetData(self):
+        self.targets = []
 
 
 def distance(p1, p2):
-	return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
 def undistortImage(imageProc, image):
-	""" Removes distortion from image by applying intrinsic matrix and distortion coefficients
-	
-	Note: ImageProcessor must have intrinsic parameters already loaded
-	Arguments:
-		imageProc -- ImageProcessor object
-		image -- Input image (distorted)
-		
-	Returns undistorted image
-	
-	"""
-	
-	return cv.remap(image, imageProc.map1, imageProc.map2, cv.INTER_LINEAR)
+    """ Removes distortion from image by applying intrinsic matrix and distortion coefficients
+    
+    Note: ImageProcessor must have intrinsic parameters already loaded
+    Arguments:
+        imageProc -- ImageProcessor object
+        image -- Input image (distorted)
+        
+    Returns undistorted image
+    
+    """
+    
+    return cv.remap(image, imageProc.map1, imageProc.map2, cv.INTER_LINEAR)
 
 
 def convertToGlobal(imageProc, input):
-	""" Converts x and y coordinates to global coordinates, according to calibration parameters
-	
-	Arguments:
-		imageProc -- ImageProcessor object
-		input -- x and y coordinates of input point in a list
-		
-	Returns a 3D point in with coordinates in an array
+    """ Converts x and y coordinates to global coordinates, according to calibration parameters
+    
+    Arguments:
+        imageProc -- ImageProcessor object
+        input -- x and y coordinates of input point in a list
+        
+    Returns a 3D point in with coordinates in an array
 
-	"""
-	
-	imgPoint = np.ones( (3, 1), np.float32 )
-	imgPoint[0, 0] = input[0]
-	imgPoint[1, 0] = input[1]
-	
-	leftMat = np.linalg.inv(imageProc.rotation) * np.linalg.inv(imageProc.intrinsic) * imgPoint
-	rightMat = np.linalg.inv(imageProc.rotation) * imageProc.translation
-	s = rightMat[2,0]
-	s /= leftMat[2,0]
-	
-	return np.linalg.inv(imageProc.rotation) * ( s * np.linalg.inv(imageProc.intrinsic) * imgPoint - imageProc.translation )
-
+    """
+    
+    imgPoint = np.ones( (3, 1), np.float32 )
+    imgPoint[0, 0] = input[0]
+    imgPoint[1, 0] = input[1]
+    
+    leftMat = np.linalg.inv(imageProc.rotation) * np.linalg.inv(imageProc.intrinsic) * imgPoint
+    rightMat = np.linalg.inv(imageProc.rotation) * imageProc.translation
+    s = rightMat[2,0]
+    s /= leftMat[2,0]
+    
+    return np.linalg.inv(imageProc.rotation) * ( s * np.linalg.inv(imageProc.intrinsic) * imgPoint - imageProc.translation )
--- processors/image.py
+++ processors/image.py
@@ -13,188 +13,180 @@ DETECT_MIN = np.array([0, 154, 109], np.uint8)
 DETECT_MAX = np.array([37, 255, 255], np.uint8)
 
 class ImageProcessor(object):
-	# Frames to conditionally display
-	frame_types = ('main', 'orig', 'blur', 'avg', 'gray', 'bw')
+    # Frames to conditionally display
+    frame_types = ('main', 'orig', 'blur', 'avg', 'gray', 'bw')
 
-	def __init__(self, img_source, frame_type=0, data_proc=None, position='left', x_offset=0, y_offset=0):
-		self.img_source = img_source
-		self.last_frame = None
-		self.__avg_frame = None
-		self.frame_type = self.frame_types[frame_type]
-		self.coverage_size = [17, 10]
-		self.coverage_offset = [0, 0]
-		self.position = position
-		self.x_offset = x_offset
-		self.y_offset = y_offset
-		self.cal_data = calibrationData()
-		
-		# Temporary polynomial variables
-		self.A = 0.4386 #0.1916
-		self.B = 1.6007 #1.7223
-		self.C = -0.0615 #-0.0754
+    def __init__(self, img_source, frame_type=0, data_proc=None, position='left', x_offset=0, y_offset=0):
+        self.img_source = img_source
+        self.last_frame = None
+        self.__avg_frame = None
+        self.frame_type = self.frame_types[frame_type]
+        self.coverage_size = [17, 10]
+        self.coverage_offset = [0, 0]
+        self.position = position
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.cal_data = calibrationData()
+        
+        # Temporary polynomial variables
+        self.A = 0.4386 #0.1916
+        self.B = 1.6007 #1.7223
+        self.C = -0.0615 #-0.0754
 
-		if data_proc is None:
-			self.data_proc = DataProcessor()
-		else:
-			self.data_proc = data_proc
-	
-	def process(self):
-		self.last_frame = self.img_source.read()
-		if self.avg_frame is None:
-			self.avg_frame = self.last_frame
-		#self.last_frame, self.avg_frame = processImage(self.last_frame, self.avg_frame, self.frame_type)
-		self.last_frame, self.avg_frame, img_data = findObjects(self.last_frame, self.avg_frame, self.frame_type)
+        if data_proc is None:
+            self.data_proc = DataProcessor()
+        else:
+            self.data_proc = data_proc
+    
+    def process(self):
+        self.last_frame = self.img_source.read()
+        if self.avg_frame is None:
+            self.avg_frame = self.last_frame
+        #self.last_frame, self.avg_frame = processImage(self.last_frame, self.avg_frame, self.frame_type)
+        self.last_frame, self.avg_frame, img_data = findObjects(self.last_frame, self.avg_frame, self.frame_type)
 
-		self.data_proc.process(img_data, self)
+        self.data_proc.process(img_data, self)
 
-		return self.last_frame
+        return self.last_frame
 
-	@property
-	def avg_frame(self):
+    @property
+    def avg_frame(self):
                 #define average frame property to be used in process
-		return self.__avg_frame
-	@avg_frame.setter
-	def avg_frame(self, frame):
+        return self.__avg_frame
+    @avg_frame.setter
+    def avg_frame(self, frame):
                 #set average frame using numpy float
-		if self.__avg_frame is None:
-			self.__avg_frame = np.float32(frame)
-		else:
-			self.__avg_frame = frame
+        if self.__avg_frame is None:
+            self.__avg_frame = np.float32(frame)
+        else:
+            self.__avg_frame = frame
 
-	def saveFrame(self, filename=""):
-		self.img_source.save(filename, self.last_frame)
+    def saveFrame(self, filename=""):
+        self.img_source.save(filename, self.last_frame)
 
-	def setFrameType(self, frame_type):
-		if isinstance(frame_type, basestring):
-			if frame_type in self.frame_types:
-				self.frame_type = frame_type
-			else:
-				raise Exception("Invalid frame type '%s'" % frame_type)
-		else:
-			self.frame_type = self.frame_types[frame_type]
-	
-	def setCoverageOffset(self, root):
-		inputs = {'Coverage Offset X:': self.coverage_offset[0], 'Coverage Offset Y:': self.coverage_offset[1]}
-		data = InputDialog(root, ('%s Coverage Offset' % self.img_source.name), inputs).result
-		if data:
-			self.coverage_offset = [float(data['Coverage Offset X:']), float(data['Coverage Offset Y:'])]
-			logging.info("Coverage offset: %s" % self.coverage_offset)
+    def setFrameType(self, frame_type):
+        if isinstance(frame_type, basestring):
+            if frame_type in self.frame_types:
+                self.frame_type = frame_type
+            else:
+                raise Exception("Invalid frame type '%s'" % frame_type)
+        else:
+            self.frame_type = self.frame_types[frame_type]
+    
+    def setCoverageOffset(self, root):
+        inputs = {'Coverage Offset X:': self.coverage_offset[0], 'Coverage Offset Y:': self.coverage_offset[1]}
+        data = InputDialog(root, ('%s Coverage Offset' % self.img_source.name), inputs).result
+        if data:
+            self.coverage_offset = [float(data['Coverage Offset X:']), float(data['Coverage Offset Y:'])]
+            logging.info("Coverage offset: %s" % self.coverage_offset)
 
-	def setCoverageSize(self, root):
-		inputs = {'Coverage Width:': self.coverage_size[0], 'Coverage Range:': self.coverage_size[1]}
-		data = InputDialog(root, ('%s Coverage Size' % self.img_source.name), inputs).result
-		if data:
-			self.coverage_size = [float(data['Coverage Width:']), float(data['Coverage Range:'])]
-			logging.info("Coverage size: %s" % self.coverage_size)
-	
-	def setPolynomials(self, root):
-		inputs = {'A': self.A, 'B': self.B, 'C': self.C}
-		data = InputDialog(root, ('%s Polynomial Vars' % self.img_source.name), inputs).result
-		if data:
-			[self.A, self.B, self.C] = [float(data['A']), float(data['B']), float(data['C'])]
-			logging.info("Polynomials: %d + %dx + %dx^2" % (self.A, self.B, self.C))
-			
-	def setPosition(self, position):
-		""" Sets position according to corresponding camera placement
-		
-		Arguments:
-			position -- camera position (left OR right)
-			
-		"""
-		
-		self.position = position
-		
+    def setCoverageSize(self, root):
+        inputs = {'Coverage Width:': self.coverage_size[0], 'Coverage Range:': self.coverage_size[1]}
+        data = InputDialog(root, ('%s Coverage Size' % self.img_source.name), inputs).result
+        if data:
+            self.coverage_size = [float(data['Coverage Width:']), float(data['Coverage Range:'])]
+            logging.info("Coverage size: %s" % self.coverage_size)
+    
+    def setPolynomials(self, root):
+        inputs = {'A': self.A, 'B': self.B, 'C': self.C}
+        data = InputDialog(root, ('%s Polynomial Vars' % self.img_source.name), inputs).result
+        if data:
+            [self.A, self.B, self.C] = [float(data['A']), float(data['B']), float(data['C'])]
+            logging.info("Polynomials: %d + %dx + %dx^2" % (self.A, self.B, self.C))
+            
+    def setPosition(self, position):
+        """ Sets position according to corresponding camera placement
+        
+        Arguments:
+            position -- camera position (left OR right)
+            
+        """
+        
+        self.position = position
+        
+    def loadIntrinsicParams(self):
+        """ Loads intrinsic matrix and distortion coefficients from xml files into ImageProcessor object, and calculates distortion map
+        
+        """
+
+        ### TODO Possibly add files to config
+        self.cal_data.intrinsic = np.loadtxt(
+            os.path.join('calibration_data', 'intrinsics.txt'))
+        self.cal_data.distortion = np.loadtxt(
+            os.path.join('calibration_data', 'distortion.txt'))
+        
+        
+    def calcDistortionMaps(self):
+        """ Calculates distortion maps 
+            
+        """
+        
+        size = self.img_source.height, self.img_source.width
+        
+        # Calculate newCameraMatrix
+        newCameraMatrix, newExtents = cv.getOptimalNewCameraMatrix(self.cal_data.intrinsic, self.cal_data.distortion, size, 0.0)
+        
+        # Calculate Distortion Maps
+        self.cal_data.map1, self.cal_data.map2 = cv.initUndistortRectifyMap(self.cal_data.intrinsic, self.cal_data.distortion, None, newCameraMatrix, size, cv.CV_32FC1)
+
+    def __string__(self):
+        return 'Image Processor{%r}' % self.image_source
+    def __repr__(self):
+        return self.__string__()
 
 def findObjects(frame, avg_frame, frame_type=ImageProcessor.frame_types[0], detectMin=DETECT_MIN, detectMax=DETECT_MAX):
-	blur_frame = cv.GaussianBlur(frame, (19, 19), 0)
-	hsv_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV)
-	thresh_frame = cv.inRange(hsv_frame, detectMin, detectMax)
+    blur_frame = cv.GaussianBlur(frame, (19, 19), 0)
+    hsv_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV)
+    thresh_frame = cv.inRange(hsv_frame, detectMin, detectMax)
 
-	# Calculate contours
-	bw_copy = thresh_frame.copy()
-	contours, hier = cv.findContours(bw_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Calculate contours
+    bw_copy = thresh_frame.copy()
+    contours, hier = cv.findContours(bw_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-	# Draw countours and bounding boxes
-	main_frame = frame.copy()
-	for contour in contours:
-		rect = cv.boundingRect(contour)
-		top  = rect[0:2]
-		bot  = (rect[0] + rect[2], rect[1] + rect[3])
-		cv.rectangle(main_frame, top, bot, (255, 0, 0), 1)
-	cv.drawContours(main_frame, contours, -1, (0, 255, 0), -1)
+    # Draw countours and bounding boxes
+    main_frame = frame.copy()
+    for contour in contours:
+        rect = cv.boundingRect(contour)
+        top  = rect[0:2]
+        bot  = (rect[0] + rect[2], rect[1] + rect[3])
+        cv.rectangle(main_frame, top, bot, (255, 0, 0), 1)
+    cv.drawContours(main_frame, contours, -1, (0, 255, 0), -1)
 
-	frames = dict(zip(ImageProcessor.frame_types, (main_frame, frame, thresh_frame)))
-	out_frame = frames[frame_type]
+    frames = dict(zip(ImageProcessor.frame_types, (main_frame, frame, thresh_frame)))
+    out_frame = frames[frame_type]
 
 
-	return (out_frame, avg_frame, contours)
-	
+    return (out_frame, avg_frame, contours)
+    
 def processImage(frame, avg_frame, frame_type=ImageProcessor.frame_types[0]):
-	# Blur and average with previous frames
-	src_frame = cv.GaussianBlur(frame, (19, 19), 0)
-	cv.accumulateWeighted(src_frame, avg_frame, AVG_WEIGHT)
-	conv_frame = cv.convertScaleAbs(avg_frame)
+    # Blur and average with previous frames
+    src_frame = cv.GaussianBlur(frame, (19, 19), 0)
+    cv.accumulateWeighted(src_frame, avg_frame, AVG_WEIGHT)
+    conv_frame = cv.convertScaleAbs(avg_frame)
 
-	# Subtract current and average frames
-	diff_frame = cv.absdiff(src_frame, conv_frame)
+    # Subtract current and average frames
+    diff_frame = cv.absdiff(src_frame, conv_frame)
 
-	# Convert to grayscale then to black/white
-	gray_frame = cv.cvtColor(diff_frame, cv.COLOR_RGB2GRAY)
-	_, bw_frame = cv.threshold(gray_frame, BW_THRESHOLD, 255, cv.THRESH_BINARY)
+    # Convert to grayscale then to black/white
+    gray_frame = cv.cvtColor(diff_frame, cv.COLOR_RGB2GRAY)
+    _, bw_frame = cv.threshold(gray_frame, BW_THRESHOLD, 255, cv.THRESH_BINARY)
 
-	# Calculate contours
-	bw_copy = bw_frame.copy()
-	contours, hier = cv.findContours(bw_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Calculate contours
+    bw_copy = bw_frame.copy()
+    contours, hier = cv.findContours(bw_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-	# Draw countours and bounding boxes
-	main_frame = frame.copy()
-	for contour in contours:
-		rect = cv.boundingRect(contour)
-		top  = rect[0:2]
-		bot  = (rect[0] + rect[2], rect[1] + rect[3])
-		cv.rectangle(main_frame, top, bot, (255, 0, 0), 1)
-	cv.drawContours(main_frame, contours, -1, (0, 255, 0), -1)
+    # Draw countours and bounding boxes
+    main_frame = frame.copy()
+    for contour in contours:
+        rect = cv.boundingRect(contour)
+        top  = rect[0:2]
+        bot  = (rect[0] + rect[2], rect[1] + rect[3])
+        cv.rectangle(main_frame, top, bot, (255, 0, 0), 1)
+    cv.drawContours(main_frame, contours, -1, (0, 255, 0), -1)
 
-	# Select desired frame to display
-	frames = dict(zip(ImageProcessor.frame_types, (main_frame, frame, src_frame, conv_frame, gray_frame, bw_frame)))
-	out_frame = frames[frame_type]
+    # Select desired frame to display
+    frames = dict(zip(ImageProcessor.frame_types, (main_frame, frame, src_frame, conv_frame, gray_frame, bw_frame)))
+    out_frame = frames[frame_type]
 
-	return (out_frame, avg_frame)
-
-	def __string__(self):
-		return 'Image Processor{%r}' % self.image_source
-	def __repr__(self):
-		return self.__string__()
-	
-	
-def loadIntrinsicParams(imageProc):
-	""" Loads intrinsic matrix and distortion coefficients from xml files into ImageProcessor object, and calculates distortion map
-	
-	Arguments:
-		imageProc -- ImageProcessor object
-	
-	"""
-
-	### TODO Possibly add files to config
-	imageProc.cal_data.intrinsic = np.loadtxt(
-        os.path.join('calibration_data', 'intrinsics.txt'))
-	imageProc.cal_data.distortion = np.loadtxt(
-        os.path.join('calibration_data', 'distortion.txt'))
-	
-	
-def calcDistortionMaps(imageProc):
-	""" Calculates distortion maps 
-	
-	Arguments:
-		imageProc -- ImageProcessor object
-		
-	"""
-	
-	size = imageProc.img_source.height, imageProc.img_source.width
-	
-	# Calculate newCameraMatrix
-	newCameraMatrix, newExtents = cv.getOptimalNewCameraMatrix(imageProc.cal_data.intrinsic, imageProc.cal_data.distortion, size, 0.0)
-	
-	# Calculate Distortion Maps
-	imageProc.cal_data.map1, imageProc.cal_data.map2 = cv.initUndistortRectifyMap(imageProc.cal_data.intrinsic, imageProc.cal_data.distortion, None, newCameraMatrix, size, cv.CV_32FC1)
+    return (out_frame, avg_frame)
 
