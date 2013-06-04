61f9a542b48a4d36362189b5aba2a0fe608ab060
Nick Luzod <nluzod@gmail.com>
2013-06-03 19:42:05 -0700

    Added structs module

---
+++ processors/structs.py
@@ -0,0 +1,12 @@
+
+class calibrationData(object):
+    
+    def __init__(self):
+        
+        self.intrinsic = None
+        self.distortion = None
+        self.map1 = None
+        self.map2 = None
+        self.rotation = None
+        self.translation = None
+        
