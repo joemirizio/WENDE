eb7b4164e7b8d0dec1366d46bbe6f6885baa6633
boogers <deanjac@gmail.com>
2013-07-16 17:27:39 -0400

    Update correlation.py
    Added documentation.

--- processors/data/correlation.py
+++ processors/data/correlation.py
@@ -1,80 +1,70 @@
-import itertools
+"""
+Analyzes detected targets from all system image processors and determines
+whether identified targets are unique or reference the same object. For
+instance, this functionality allows the system to recognize a target as a
+single object even while viewed by multiple image sources simultaneously.
+
+Classes:
+TargetCorrelationModule
+"""
 import math
 import numpy as np
 
+
 class TargetCorrelationModule(object):
+    """Analyzes detected targets from all system image processors and
+    determines whether identified targets are unique or reference the same
+    object. This is done by comparing their relative proximity.
+
+    Attributes:
+    image_processor: An ImageProcessor object.
+
+    Methods:
+    checkUnique()
+    """
     def __init__(self, image_processor):
         self.image_processor = image_processor
-    
+
     def checkUnique(self, image_processors):
+        """Reads in one or more ImageProcessor objects and determines whether
+        identified targets in these image processors are unique or reference
+        the same object.
+
+        Detections located within 0.5 feet of each other are assumed to
+        reference the same object. In this case, the two target postions are
+        averaged into a single coordinate and added to the unique positions
+        list. The unique positions list stores the coordinates of all targets
+        identified as unique across all image processors.
+
+        Args:
+            A list of image processor objects.
+
+        Returns:
+            A list of 2-D coordinates.
+        """
+
         from data import distance
-        #for proc in image_processors:
-            # Return if calibration is not completed
-            #if not proc.cal_data.is_valid:
-                #return []
-
-        #uniqueTarg = [] 
-        #proc1Points = []
-        #proc2Points = []
-        
-        ## Create 2-element permutations of all image processors
-        #imgProcPermutations = list(itertools.permutations(positions, 2))
-        
-        ## Remove all  reverse order permutations, i.e. remove BA if AB exists  
-        #for i, _ in enumerate(imgProcPermutations):
-            #reverseOrder = (imgProcPermutations[i][1], imgProcPermutations[i][0])
-            #for entry in imgProcPermutations:
-                #if entry == reverseOrder:
-                      #imgProcPermutations.remove(reverseOrder)
-
-        ## Convert all points in image proc pair to global coordinates
-        #for imgProcPair in imgProcPermutations:
-            ## Cycle through each coordinate in the first image processor 
-            #for point in imgProcPair[0]:
-                #point = np.array(point)
-                #point = convertToGlobal(image_processors[0], point)
-                #point = point.tolist()
-                #point = list(itertools.chain.from_iterable(point))
-                #proc1Points.append(point)
-                #uniqueTarg.append(tuple(point))
-            ## Cycle through each coordinate in the second image processor
-            #for point in imgProcPair[1]:
-                #point = np.array(point)
-                #point = convertToGlobal(image_processors[1], point)
-                #point = point.tolist()
-                #point = list(itertools.chain.from_iterable(point))
-                #proc2Points.append(point)
-                #uniqueTarg.append(tuple(point))
-            ## Create 2-element permutations of global coordinates 
-            #posPermutations = list(itertools.product(proc1Points, proc2Points))
-            ## Checks for adjacent targets and avgs their position if close
-            #for i, gblPointPair in enumerate(posPermutations):
-                #gblPoint1 = gblPointPair[0]
-                #gblPoint2 = gblPointPair[1]
-                #if distance(gblPoint1, gblPoint2) < 1:
-                    #uniqueTarg.append(tuple(np.mean((gblPoint1, gblPoint2), axis = 0)))
-                    #uniqueTarg.remove(tuple(gblPoint1))
-                    #uniqueTarg.remove(tuple(gblPoint2))
-            ## Removes duplicate points from detection list  
-            #uniqueTarg = list(set(uniqueTarg))
 
         unique_positions = []
         for image_processor in image_processors:
-            #
+            # Check for calibrated system condition
             if not image_processor.cal_data.is_valid:
                 continue
 
+            # Compare all valid targets, looking for closely adjacent pairs
             for position in image_processor.valid_targets:
-                #
                 position_matched = False
                 for i, unique_position in enumerate(unique_positions):
-                    #
+                    # Targets located within 0.5 feet of each other have their
+                    # positions averaged into a single coordinate that is
+                    # then added to the unique positions list
                     if distance(position, unique_position) < 0.5:
                         unique_positions[i] = np.mean((position,
-                                                       unique_position), axis = 0)
+                                                      unique_position), axis=0)
                         position_matched = True
                         break
-                #
+                # Targets distanced from other targets have their coordinates
+                # added to the unique positions list
                 if not position_matched:
                     unique_positions.append(position)
 
