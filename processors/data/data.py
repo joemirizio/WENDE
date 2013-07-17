94921ce0d2ff4250ee32da15a7551df15eb6b067
boogers <deanjac@gmail.com>
2013-07-16 20:21:54 -0400

    Added documentation

--- processors/data/data.py
+++ processors/data/data.py
@@ -1,3 +1,23 @@
+"""
+The data processor processes the information received from the image
+processors and provides the result to the Target Track Module.
+
+All detections from the image processors are filtered through the Target
+Discrimination Module and the Target Correlation Module. The resulting
+finalized position list is then provided to the Target Track Module
+for track assignment and updating.
+
+Additional functionality converts image coordinates into the global
+reference frame and also removes image source distortion.
+
+Classes:
+    DataProcessor
+
+Functions:
+    distance()
+    undistortImage()
+    convertToGlobal()
+"""
 import math
 import logging
 import numpy as np
@@ -15,13 +35,31 @@ POS_THRESHOLD = 0.05
 
 
 class DataProcessor(object):
+    """Processes the data received from the image processors and provides it
+    to the Target Track Module.
+
+    All detections from the image processors are filtered through the Target
+    Discrimination Module and the Target Correlation Module. The resulting
+    finalized position list is then provided to the Target Track Module
+    for track assignment and updating. A method for erasing all target data
+    is also provided.
+
+    Attributes:
+        targets: A list of stored Target objects.
+        coverages:
+        tca: A Tactical Computer Application object.
+        config: A SafeConfigParser object.
+        is_active: A boolean indicating the activity status of the data
+            processor.
+        tcm: A TargetCorrelationModule object.
+        tdm: A TargetDisciminationModule object.
+        ttm: A TargetTrackModule object.
+
+    Methods:
+        process()
+        clearTargetData()
+        toggleActive()
     """
-    Top level class for processing position data. Handles prediction,
-    correlation, and track.
-    
-    Args: None
-    """
-    
     def __init__(self, tca):
         self.targets = []
         self.coverages = {}
@@ -36,18 +74,20 @@ class DataProcessor(object):
         self.ttm = TargetTrackModule(self)
 
     def process(self):
+        """Filters all detections from the image processors through the Target
+        Discrimination Module and the Target Correlation Module. The resulting
+        finalized position list is then provided to the Target Track Module
+        for track assignment and updating.
+
+        NOTE: The is_active attribute must be True for processing to occur.
+
+        Args:
+            None
         """
-        Process the next set of calibrated data from the discrimination module.
-        
-        Args: None
-        """
-        
         # Only process if active
         if not self.is_active:
             return
 
-        filtered_positions = []
-        
         for image_processor in self.tca.image_processors:
             # Only process if calibrated
             if not image_processor.cal_data.is_valid:
@@ -57,14 +97,13 @@ class DataProcessor(object):
             self.tdm.discriminate(image_processor.last_detected_positions,
                                   image_processor)
 
-       
-#        [[[x1,y1],[x2,y2],[x3,y3]],...]
-
+        # Filter position list to obtain only unique targets
         unique_positions = self.tcm.checkUnique(self.tca.image_processors)
 
         logging.debug("-" * 20)
         logging.debug("UNIQUE POSITIONS: %s" % unique_positions)
 
+        # Store finalized list of targets and assign/update tracks
         self.ttm.processDetections(unique_positions)
 
         # TODO Clean reference up
@@ -74,70 +113,84 @@ class DataProcessor(object):
             logging.debug("TARGET: %s" % target)
 
     def clearTargetData(self):
+        """Erases all stored target data.
+
+        Args:
+            None
         """
-        Clear all track data from the Target
-        track module.
-        
-        Args: None
-        """
-        
         del self.ttm.targets[:]
         del self.targets[:]
 
     def toggleActive(self):
+        """A boolean indicator that toggles the activity status of the data
+        processor.
+
+        Args:
+            None
+        """
         self.is_active = not self.is_active
 
-# TODO Possible move 
+
 def distance(p1, p2):
+    """Calculates the distance between a pair of 2-D coordinates.
+
+    Args:
+        p1: A 2-D coordinate.
+        p2: A 2-D coordinate.
+
+    Returns:
+        A float.
+    """
     return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
+
 def undistortImage(imageProc, image):
-    """ Removes distortion from image by applying intrinsic matrix and distortion coefficients
-    
-    Note: ImageProcessor must have intrinsic parameters already loaded
-    Arguments:
-        imageProc -- ImageProcessor object
-        image -- Input image (distorted)
-        
-    Returns undistorted image
-    
+    """Removes distortion from image by applying intrinsic matrix and
+    distortion coefficients.
+
+    Note: ImageProcessor must have intrinsic parameters already loaded.
+
+    Args:
+        imageProc: An ImageProcessor object.
+        image: A distorted 8-bit image array.
+
+    Returns:
+        An undistorted 8-bit image array.
     """
-    
     return cv2.remap(image, imageProc.map1, imageProc.map2, cv2.INTER_LINEAR)
 
 
 def convertToGlobal(imageProc, coordinates):
-    """ Converts x and y coordinates to global coordinates, according to calibration parameters
-    
-    Arguments:
-        imageProc -- ImageProcessor object
-        coordinates -- x and y coordinates of input point in an array-like object
-        
-    Returns a 3D point in with coordinates in an array
+    """Converts a 2-D image coordinate to a 2-D global coordinate, according
+    to calibration parameters.
 
+    Args:
+        imageProc: An ImageProcessor object.
+        coordinates: A 2-D image coordinate.
+
+    Returns:
+        A 2-D global coordinate.
     """
-    
     # Convert to numpy array
     coordinates = np.array(coordinates)
 
-    imgPoint = np.ones( (3, 1), np.float32 )
+    imgPoint = np.ones((3, 1), np.float32)
     imgPoint[0, 0] = coordinates[0]
     imgPoint[1, 0] = coordinates[1]
-    
+
     # Convert to matrix to simplify the following linear algebra
-    # TODO: CLEAN THIS UP
     imgPoint = np.matrix(imgPoint)
     intrinsic = np.matrix(imageProc.cal_data.intrinsic)
     rotation = np.matrix(imageProc.cal_data.rotation)
     translation = np.matrix(imageProc.cal_data.translation)
-    
+
     leftMat = np.linalg.inv(rotation) * np.linalg.inv(intrinsic) * imgPoint
     rightMat = np.linalg.inv(rotation) * translation
-    s = rightMat[2,0]
-    s /= leftMat[2,0]
-    
-    position = np.array(np.linalg.inv(rotation) * 
-        (s * np.linalg.inv(intrinsic) * imgPoint - translation))[:2]
+    s = rightMat[2, 0]
+    s /= leftMat[2, 0]
+
+    position = np.array(np.linalg.inv(rotation)*(s * np.linalg.inv(intrinsic)
+                                               * imgPoint - translation))[:2]
 
     # Convert from numpy to list
     from display.tactical.tactical import flattenArray
